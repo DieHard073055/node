2f718b6a61c19dcb47e2cf2998f43e336a3e9daf
DieHard073055 <eshanshafeeq073055@gmail.com>
2019-01-24 22:11:59 +1100

    added config manager for node

---
+++ src/utils/config_manager.py
@@ -0,0 +1,11 @@
+class ConfigManager(object):
+    def __init__(self):
+        print('initiated config manager')
+
+    def validate_config(self):
+        print('config parsed')
+
+    def save_config(self):
+        print('config file saved')
+
+
