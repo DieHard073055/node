89238aaa135b211005180fb21e0cbbb46612463e
DieHard073055 <eshan.shafeeq@switchdin.com>
2019-01-26 01:55:37 +1100

    added test spec command read type for health metrics

--- src/test/drivers/sensors/test_health_monitor.py
+++ src/test/drivers/sensors/test_health_monitor.py
@@ -23,6 +23,14 @@ class TestHealthMonitor(unittest.TestCase):
             actual_cpu_temp = {read_cpu_temp_command['key']:cpu_temp_file.read()}
         self.assertEqual(cpu_temp, actual_cpu_temp)
 
+    def testResolveCommand_typeRead(self):
+        read_meminfo = {'key': 'meminfo', 'type': 'read', 'command': ['/proc/meminfo']}
+        meminfo = self.hm.resolve_request(read_meminfo)
+        actual_meminfo = {}
+        with open(read_meminfo['command'][0], 'r') as memfile:
+            actual_meminfo = {read_meminfo['key']: memfile.read()}
+        self.assertEqual(meminfo, actual_meminfo)
+
 
 
 if __name__ == '__main__':
