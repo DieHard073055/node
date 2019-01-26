a006cc4e48f10d896ca02d1aafab25589cf1f2e6
DieHard073055 <eshanshafeeq073055@gmail.com>
2019-01-26 14:01:16 +1100

    half way refactoring test_health_monitor

--- src/test/drivers/sensors/test_health_monitor.py
+++ src/test/drivers/sensors/test_health_monitor.py
@@ -14,31 +14,53 @@ class TestHealthMonitor(unittest.TestCase):
         self.config = saved_configs.get_config()['drivers'][0]['params']
         self.hm = health_monitor.HealthMonitor(self.config)
 
-
     def testResolveCommand_typeShell(self):
-        read_cpu_temp_command = {'key': 'cpu_temp_0', 'type': 'shell', 'command': ['cat', '/sys/class/thermal/thermal_zone0/temp']}
-        cpu_temp = self.hm.resolve_request(read_cpu_temp_command)
+        request = {
+            'key': 'cpu_temp_0',
+            'type': 'shell',
+            'command': ['cat', '/sys/class/thermal/thermal_zone0/temp']
+        }
+        cpu_temp = self.hm.resolve_request(request)
+        print('\ncpu temp ' + str(cpu_temp))
         actual_cpu_temp = 0
-        with open(read_cpu_temp_command['command'][1], 'r') as cpu_temp_file:
-            actual_cpu_temp = {read_cpu_temp_command['key']:cpu_temp_file.read()}
+        with open(request['command'][1], 'r') as cpu_temp_file:
+            actual_cpu_temp = {request['key']:cpu_temp_file.read()}
         self.assertEqual(cpu_temp, actual_cpu_temp)
 
     def testResolveCommand_typeRead(self):
-        read_meminfo = {'key': 'meminfo', 'type': 'read', 'command': ['/proc/meminfo']}
-        meminfo = self.hm.resolve_request(read_meminfo)
+        request = {
+            'key': 'meminfo',
+            'type': 'read',
+            'command': ['/proc/meminfo']
+        }
+        meminfo = self.hm.resolve_request(request)
         actual_meminfo = {}
-        with open(read_meminfo['command'][0], 'r') as memfile:
-            actual_meminfo = {read_meminfo['key']: memfile.read()}
+        with open(request['command'][0], 'r') as memfile:
+            actual_meminfo = {'{}_0'.format(request['key']): memfile.read()}
         self.assertEqual(meminfo, actual_meminfo)
 
     def testResolveCommand_typeShellPIPE(self):
-        request = self.config[1]
+        request = {
+            'key': 'mem_available',
+            'type': 'shell',
+            'command': [
+                'cat',
+                '/proc/meminfo',
+                '|',
+                'grep',
+                'MemAvailable',
+                '|',
+                "awk '{print $2}'"
+            ]
+        }
         memavailable = self.hm.resolve_request(request)
+        print('\nmemavai : ' + str(memavailable) )
+        actual_memavailable = None
+        with open(request['command'][1], 'r') as memfile:
+            actual_memeavailable = [line for line in memfile.read().split('\n') if 'MemAvailable:' in line.split(' ')][0].split(' ')[3]
+        print( '-----------' + str(actual_memavailable) )
         self.assertEqual(True, False)
 
         
-
-
-
 if __name__ == '__main__':
     unittest.main()
