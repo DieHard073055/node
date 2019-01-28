0fd5d468dcfb470597cd52ae234dff8d2330df42
DieHard073055 <eshanshafeeq073055@gmail.com>
2019-01-28 22:41:28 +1100

    refactored and added tests for integrating modules via config

--- src/test/test_node.py
+++ src/test/test_node.py
@@ -1,14 +1,12 @@
 """ tests which dictate the specs for the module node """
 import unittest
-from unittest import TestCase
-from unittest.mock import patch
+import unittest.mock
 import node
 
 '''
  mock for the config manager class
 '''
-
-exec_methods = []
+exec_methods = []   # keep track of the methods thats being called.
 class MockConfigManager(object):
     def __init__(self):
         global exec_methods
@@ -21,13 +19,45 @@ class MockConfigManager(object):
         exec_methods.append('save_config')
 
 
-class TestNodeStartBehaviour(TestCase):
-    @patch('node.config_manager.ConfigManager', autospec = True)
-    def test_node_starting(self, mcm):
+class TestNodeStartBehaviour(unittest.TestCase):
+    @unittest.mock.patch('node.config_manager.ConfigManager', autospec = True)
+    def testMain_normalOperation(self, mcm):
         global exec_methods
         mcm.side_effect = MockConfigManager
         node.main()
         self.assertListEqual(exec_methods, ['__init__', 'validate_config', 'save_config'])
+
+class TestNodeModuleImporter(unittest.TestCase):
+    def setUp(self):
+        health_monitor = 'drivers.sensors.health_monitor'
+        config_params = []
+        self.config = {
+            "drivers":[
+                {
+                    "module":health_monitor,
+                    "params":config_params
+                }
+            ],
+
+        }
+        self.non_existent_config = {
+            "drivers":[
+                {
+                    "module":"non_existent.non_existent",
+                    "params":config_params
+                }
+            ],
+
+        }
+
+    def testImportModule_HealthMonitor(self):
+        for driver in self.config['drivers']:
+            node.initialize_modules(driver)
+
+
+    def testImportModule_NonExistantModule(self):
+        for driver in self.non_existent_config['drivers']:
+            node.initialize_modules(driver)
 
 if  __name__ == '__main__':
     unittest.main()
