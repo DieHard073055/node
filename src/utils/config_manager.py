235af694ba3d1e4edf45df18cdc01f7b7d50b053
DieHard073055 <eshanshafeeq073055@gmail.com>
2019-01-29 23:38:42 +1100

    added load config to load the sample config file

--- src/utils/config_manager.py
+++ src/utils/config_manager.py
@@ -1,5 +1,7 @@
+import json
 class ConfigManager(object):
     def __init__(self):
+        self.config_file = 'sample_config.json'
         print('initiated config manager')
 
     def validate_config(self):
@@ -7,5 +9,11 @@ class ConfigManager(object):
 
     def save_config(self):
         print('config file saved')
+
+    def load_config(self):
+        config = None
+        with open(self.config_file) as config_file:
+            config = json.loads(config_file.read())
+        return config
 
 
